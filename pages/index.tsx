230d1746fdaa9c6e6aa3df14a0989a0d0c3a844f
Alfonso M. García Astorga <me@alfon.io>
2020-09-15 12:33:39 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
+    <Container maxWidth="sm" style={{ height: "100vh" }}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        my={4}
+        height="100%"
+      >
+        aa
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js with TypeScript example
         </Typography>
